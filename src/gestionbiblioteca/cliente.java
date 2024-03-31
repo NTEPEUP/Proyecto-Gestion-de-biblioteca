2ff829ec11027af86fe7d364421b2a74dee92230
Nelson <Nelson@MSI>
2024-03-31 11:40:27 -0600

    estableciendo conexion con la base de datos

---
+++ src/gestionbiblioteca/cliente.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package gestionbiblioteca;
+
+import java.beans.beancontext.BeanContextMembershipEvent;
+
+/**
+ *
+ * @author Nelson
+ */
+public class cliente {
+    private String nombre;
+    private String direccion;
+    private String telefono;
+    private String identifiacion;
+    
+}
